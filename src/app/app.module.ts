892e39bdf26c5660c7ebd22a4654b85f7688cbe5
elad1978 <efrathalev@gmail.com>
2023-05-10 18:57:23 +0300

    update app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { MenuComponent } from './layout/menu/menu.component';
+import { ContentComponent } from './layout/content/content.component';
+import { HomeComponent } from './screens/home/home.component';
+import { CatalogComponent } from './screens/catalog/catalog.component';
+import { ItemDetailsComponent } from './screens/item-details/item-details.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenuComponent,
+    ContentComponent,
+    HomeComponent,
+    CatalogComponent,
+    ItemDetailsComponent
   ],
   imports: [
     BrowserModule,
